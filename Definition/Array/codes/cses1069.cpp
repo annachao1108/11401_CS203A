bc6a16f77968f71ad21ef9bca756132c7eff10f7
wan-yun chao <126858731+annachao1108@users.noreply.github.com>
2026-01-04 19:55:22 +0800

    Add cses1069.cpp file with initial includes

---
+++ Definition/Array/codes/cses1069.cpp
@@ -0,0 +1,23 @@
+//string is a special character array
+#include <algorithm>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string str;
+    getline(cin, str);
+    int max=1,temp=1;
+    for(int i=1;i<str.size();i++){
+        if(str[i]==str[i-1]&&str[i]!=str[i+1]){
+            temp++;
+            if(temp>max){
+                max=temp;
+            }
+            temp=1;
+        }
+        else if (str[i]==str[i-1]){
+            temp++;
+        }
+    }
+    cout<<max<<"\n";
+}
